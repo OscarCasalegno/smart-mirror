19b4e2be9a3fcb46c81740095caad61eeb096f1b
lucad <s244685@studenti.polito.it>
2021-12-05 09:10:47 +0100

    Second Modification

--- website/routes.py
+++ website/routes.py
@@ -10,6 +10,10 @@ from flask_login import login_user, logout_user, login_required, current_user
 def home_page():
     return render_template('home.html')
 
+@app.route('/img')
+def img_page():
+    return render_template('temp.jpg')
+
 @app.route('/website', methods=['GET', 'POST'])
 @login_required
 def market_page():
